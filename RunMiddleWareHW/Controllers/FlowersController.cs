0803630cb19c45e511af66aa046566dedb02093c
rotemdan267 <rotemdan267@gmail.com>
2022-05-23 02:24:06 +0300

    Add project files.

---
+++ RunMiddleWareHW/Controllers/FlowersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowersHW.Controllers
+{
+    [ApiController]
+    [Route("flower/[action]")]
+    public class FlowersController : ControllerBase
+    {
+        public string Rose()
+        {
+            return "Roses are red...";
+        }
+
+        public string Violet()
+        {
+            return "Violets are blue...";
+        }
+
+        public string Lily()
+        {
+            return "Lily";
+        }
+
+        public string Tulip()
+        {
+            return "Tulip";
+        }
+    }
+}
